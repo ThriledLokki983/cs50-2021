fd149a461f9f0603213aec322e30e06fe31b74af
Nimoh Gideon <58972514+ThriledLokki983@users.noreply.github.com>
2021-01-15 08:19:09 +0100

    lab1

--- web-dev/cs50-2021/C-Program/week1/lab1/population.c
+++ web-dev/cs50-2021/C-Program/week1/lab1/population.c
@@ -5,11 +5,8 @@
 int main(void)
 {
  // A program to simulate population growth.
-int pop_start;
-int pop_end;
+int pop_start, pop_end,  new_pop;
 int count = 0; 
-int new_pop;
-int new_number = 0;
 
 // Prompt user for the Start size of the population // ensure that the users input is at least 9
 do
@@ -27,22 +24,25 @@ do
 // Calculate how many years require for the change in population size
 // n = n + (n/3) - (n/4) ==> new population
 
-for (int i = 0; i <= pop_start; i++)  
+/*for (int i = 0; i <= pop_start; i++)  
+
 {
- new_pop = pop_start + ((pop_start/3) - (pop_start/4));
+ new_pop = pop_start + round((pop_start/3) - (pop_start/4));
  if (new_pop <= pop_end) 
  {
   count++;
   pop_start = new_pop;
  }
+}*/
+
+while (new_pop < pop_end)
+{
+ new_pop = pop_start + round((pop_start/3) - (pop_start/4));
+ count++;
+ pop_start = new_pop;
 }
-printf("Years: %i\n", count);
-
-
-/* 
-*/
 
 // Print out the number of years it will take for the start size to get to the end size.
-
+printf("Years: %i\n", count);
  return 0;
 }
